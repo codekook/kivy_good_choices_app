d0a7555054f1b45e8c402bd34714eab18d5f49ae
codekook <ralph.corey.1@gmail.com>
2023-04-01 09:58:40 -0700

    Separated functionality and added .kv file

--- main.py
+++ main.py
@@ -1,34 +1,50 @@
-from kivy.app import App 
+'''
+
+Simple application for creating chores and tracking their completion
+
+'''
+
+from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.togglebutton import ToggleButton
-from kivy.properties import NumericProperty, ObjectProperty, StringProperty
+from kivy.uix.button import Button
+from kivy.properties import DictProperty, StringProperty
+
+class GoodChoicesWidget(BoxLayout):
+
+    pass 
+
+class GoodChoicesChore(BoxLayout):
+
+    user_input = TextInput()
+
+    #notify me every time the button state changes
+    def callback(instance):
+        print('The button <%s> is being pressed' % instance.user_input)
+
+
+class GoodChoicesList(BoxLayout):
+
+    chore_list = DictProperty({})
+    add_to_chorelist = Button()
+    completed = ToggleButton()
+
+    def add_chore(self):
+        pass
 
 class GoodChoicesApp(App):
 
     def build(self):
-        chore_layout = BoxLayout(orientation="vertical")
-        chore_input = TextInput(font_size=50, 
-                        size_hint_y=None, 
-                        multiline=False,
-                        height=200,
-                        text="Add a Chore")
-        chore_list = GridLayout(rows=2, cols=2)
-        list_item = Label(text="List of Chores",
-                        font_size=50)
-        completed_chore = ToggleButton(text="Completed", 
-                                    state="normal")
+        gc = GoodChoicesWidget()
+        self.new_user_input = GoodChoicesChore()
+        self.new_chorelist = GoodChoicesList()
+        gc.add_widget(self.new_user_input)
+        gc.add_widget(self.new_chorelist)
+        return gc
         
-        chore_input.bind(text=list_item.setter('text'))
-
-        chore_list.add_widget(list_item)
-        chore_list.add_widget(completed_chore)
-        chore_layout.add_widget(chore_input)
-        chore_layout.add_widget(chore_list)
-        return chore_layout
 
 if __name__ == "__main__":
     GoodChoicesApp().run()
