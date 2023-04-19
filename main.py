b8da64f4bc7968e8e8a406c5022f3580b89cdb78
codekook <ralph.corey.1@gmail.com>
2023-04-18 21:49:58 -0700

    Update: working initial prototype to create chores

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 from kivy.properties import AliasProperty, StringProperty, ListProperty, NumericProperty, BooleanProperty
 from kivy.clock import Clock
 
-#adds the funciontionality to create new chores and edit existing chores
+#adds the interactive functionality to each element of the graphical interface 
 class MutableTextInput(FloatLayout):
+
     text = StringProperty()
     
     def __init__(self, **kwargs):
@@ -25,20 +26,35 @@ class MutableTextInput(FloatLayout):
         Clock.schedule_once(self.prepare, 0)
     
     def prepare(self, *args):
+        #displays the chore
         self.w_label = self.ids.w_label.__self__
+
+        #makes chore editable
+        self.w_textinput = self.ids.w_textinput.__self__
+        
         self.view()
     
     def on_touch_down(self, touch):
-        pass 
+        if self.collide_point(*touch.pos) and touch.is_double_tap:
+            self.edit()
+        return super(MutableTextInput, self).on_touch_down(touch) 
 
     def edit(self):
-        pass 
+        pass
+        self.clear_widgets()
+        self.add_widget(self.w_textinput)
+        self.w_textinput.focus = True
 
     def view(self): 
-        pass 
+        self.clear_widgets() 
+        if not self.text:
+            self.w_label.tet = 'Double tap/click to edit'
+        self.add_widget(self.w_label) 
 
     def check_focus_and_view(self, textinput):
-        pass 
+        if not textinput.focus:
+            self.text = textinput.text
+            self.view()
 
 class GoodChoices(Screen):
 
@@ -60,7 +76,7 @@ class GoodChoicesChore(BoxLayout):
     chore_description = StringProperty()
 
 
-class GoodChoiceView(Screen):
+class GoodChoicesView(Screen):
 
     chore_index = NumericProperty()
     chore_description = StringProperty()
@@ -72,8 +88,9 @@ class GoodChoicesApp(App):
         self.chores = GoodChoices(name="chores")
         self.load_chores()
 
-        self.transition = SlideTransition(duration=.35)
+        self.transition = SlideTransition(duration=.25)
         root = ScreenManager(transition=self.transition)
+
         root.add_widget(self.chores)
         return root
     
@@ -85,16 +102,36 @@ class GoodChoicesApp(App):
         self.chores.data = data 
 
     def save_chores(self):
-        pass 
+        with open(self.chores_fn, 'w') as fd:
+            json.dump(self.chores.data, fd)
 
-    def del_chore(self):
-        pass 
+    def del_chore(self, chore_index):
+        del self.chores.data[chore_index]
+        self.save_chores()
+        self.refresh_chores()
+        self.go_chores()
 
-    def edit_chore(self):
-        pass 
+    def edit_chore(self, chore_index):
+        chore = self.chores.data[chore_index]
+        name = 'chore{}'.format(chore_index)
+
+        if self.root.has_screen(name):
+            self.root.remove_widget(self.root.get_screen(name)) 
+
+        view = GoodChoicesView(
+            name = name,
+            chore_index = chore_index,
+            chore_description = chore.get('description') 
+            )
+        
+        self.root.add_widget(view) 
+        self.transition.direction = 'left' 
+        self.root.current = view.name
 
     def add_chore(self):
-        pass 
+        self.chores.data.append({'description': 'New chore (double click)'})
+        chore_index = len(self.chores.data) - 1 
+        self.edit_chore(chore_index) 
 
     def set_chore_description(self, chore_index, chore_description):
         self.chores.data[chore_index]['description'] = chore_description 
@@ -102,10 +139,13 @@ class GoodChoicesApp(App):
         self.refresh_chores() 
     
     def refresh_chores(self):
-        pass 
+        data = self.chores.data 
+        self.chores.data = []
+        self.chores.data = data 
 
     def go_chores(self):
-        pass 
+        self.transition.direction = 'right'
+        self.root.current = 'chores' 
     
     @property #adds additional getter and setter functionality to chores_fn()
     def chores_fn(self):
