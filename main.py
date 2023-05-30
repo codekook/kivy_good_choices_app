cdefc6ca8d647bb56e37e13ccbe28a6201f2be74
CodeKook <ralph.corey.1@gmail.com>
2023-05-30 12:14:35 -0700

    Rebuild started on dev branch

--- main.py
+++ main.py
@@ -19,198 +19,58 @@ from kivy.clock import Clock
 from kivy.animation import Animation 
 from random import randint
 
-#adds the interactive functionality to each element of the graphical interface 
-class MutableTextInput(FloatLayout):
+class Profile():
 
-    text = StringProperty()
+    '''Class allows users to add profiles to '''
     
-    def __init__(self, **kwargs):
-        super(MutableTextInput, self).__init__(**kwargs)
-        Clock.schedule_once(self.prepare, 0)
+    def __init__(self, first, last, child):
+        self.__first = first
+        self.__last = last
+        self.__child = child
+    #Add profile
+    #Edit profile
+
+class GoodChoicesChore(Widget):
+
+    '''Class defines the attributes and behaviors associated with all chores'''
     
-    def prepare(self, *args):
-        #displays the chore
-        self.w_label = self.ids.w_label.__self__
-
-        #makes chore editable
-        self.w_textinput = self.ids.w_textinput.__self__
-        
-        self.view()
+    def __init__(self, chore, **kwargs):
+        super(GoodChoicesChore, self).__init__(**kwargs)
+        #print("chore_initialized")
+        self.__chore = chore
+        #print(self.__chore)
+        self.__complete = False
+        #print(self.__complete) 
     
-    def on_touch_down(self, touch):
-        if self.collide_point(*touch.pos) and touch.is_double_tap:
-            self.edit()
-        return super(MutableTextInput, self).on_touch_down(touch) 
+    #store a list of chores
+    #add a chore
+    #edit a chore
+    #delete a chore
+    #complete a chore- reason for the widget class inheritance
+    #reset all chores
 
-    def edit(self):
-        pass
-        self.clear_widgets()
-        self.add_widget(self.w_textinput)
-        self.w_textinput.focus = True
+class Affirmation(Widget):
 
-    def view(self): 
-        self.clear_widgets() 
-        if not self.text:
-            self.w_label.tet = 'Double tap/click to edit'
-        self.add_widget(self.w_label) 
-
-    def check_focus_and_view(self, textinput):
-        if not textinput.focus:
-            self.text = textinput.text
-            self.view()
-
-class GoodChoices(Screen):
-
-    data = ListProperty()
-    print('data: ', data)
-
-    def _get_widgets_data(self):
-        return [{
-            'chore_index': index,
-            'chore_description': item['description']
-            }
-            for index, item in enumerate(self.data)]
-
-    data_for_widgets = AliasProperty(_get_widgets_data, bind=['data']) 
-
-class GoodChoicesChore(BoxLayout):
-
-    chore_index = NumericProperty()
-    chore_description = StringProperty()
-
-class GoodChoicesView(Screen):
-
-    chore_index = NumericProperty()
-    chore_description = StringProperty()
-
-class CelebrateView(Screen):
+    '''Class contains the affirmations for completion and animates them'''
 
     pass
+    #list of affirmations
+    #animation
+
+class GoodChoicesLayout(BoxLayout):
+    pass
+
+class AffirmationLayout(BoxLayout):
+    pass 
+
 
 class GoodChoicesApp(App):
 
     def build(self):
-        self.chores = GoodChoices(name="chores")
-        self.load_chores()
-
-        self.transition = SlideTransition(duration=.25)
-        root = ScreenManager(transition=self.transition)
-
-        root.add_widget(self.chores)
+        root = GoodChoicesLayout()
+        new_chore_list = GoodChoicesChore("dishes")
+        root.add_widget(new_chore_list)
         return root
-    
-    def load_chores(self):
-        if not exists(self.chores_fn):
-            return
-        with open(self.chores_fn) as fd:
-            data = json.load(fd)
-        self.chores.data = data 
-
-    def save_chores(self):
-        with open(self.chores_fn, 'w') as fd:
-            json.dump(self.chores.data, fd)
-
-    def del_chore(self, chore_index):
-        del self.chores.data[chore_index]
-        self.save_chores()
-        self.refresh_chores()
-        self.go_chores()
-
-    def edit_chore(self, chore_index):
-        chore = self.chores.data[chore_index]
-        name = 'chore{}'.format(chore_index)
-
-        #not sure what  this check does??
-        if self.root.has_screen(name):
-            self.root.remove_widget(self.root.get_screen(name)) 
-
-        view = GoodChoicesView(
-            name = name,
-            chore_index = chore_index,
-            chore_description = chore.get('description') 
-            )
-        
-        self.root.add_widget(view) 
-        self.transition.direction = 'left' 
-        self.root.current = view.name
-
-    def add_chore(self):
-        self.chores.data.append({'description': 'New chore (double click)'})
-        chore_index = len(self.chores.data) - 1 
-        self.edit_chore(chore_index) 
-    
-    def reset_all_chores(self):
-        #iterate over all the togglebuttons
-        #retrieve the togglebutton state as complete or incomplete
-        #set value to the current state of the togglebutton
-        #if the value is equivalent to the complete state, reset the toggle button to incomplete
-        if value == "complete":
-            pass 
-
-    def set_chore_description(self, chore_index, chore_description):
-        self.chores.data[chore_index]['description'] = chore_description 
-        self.save_chores() 
-        self.refresh_chores() 
-    
-    def refresh_chores(self):
-        data = self.chores.data 
-        self.chores.data = []
-        self.chores.data = data 
-
-    def go_chores(self):
-        self.transition.direction = 'right'
-        self.root.current = 'chores' 
-    
-    @property #adds additional getter and setter functionality to chores_fn()
-    def chores_fn(self):
-        return join(self.user_data_dir, 'chores.json')
-    
-    def affirmation(self):
-        affirmations = ["Good job!", "Awesome!", "Thank you!", "Keep it up!",
-                        "Great work!", "Well done!", "Fabulous!", "Crushing it!"]
-        num = randint(0, len(affirmations) - 1)
-        self.affirm = affirmations[num]
-        return self.affirm
-    
-    def animate_it(self, instance):
-        
-        x = randint(0, self.root.width - 600)
-        y = randint(0, self.root.height - 600)
-        animation = Animation(d = 1.0, 
-                        pos = (x, y),
-                        )
-
-        x = randint(0, self.root.width - 600)
-        y = randint(0, self.root.height - 600)
-        animation += Animation(d=1.0,
-                              pos=(x, y),
-                              )
-
-        x = randint(0, self.root.width - 600)
-        y = randint(0, self.root.height - 600)
-        animation += Animation(d=1.0,
-                               pos=(x, y),
-                               )
-
-        animation += Animation(d = 1.0, 
-                        pos = (0, 600), 
-                        t = 'out_bounce'
-                        )
-
-        animation.start(instance)
-
-    def celebrate(self):
-        name = "celebrate_view"
-
-        if self.root.has_screen(name):
-            self.root.remove_widget(self.root.get_screen(name))
-
-        view = CelebrateView(name = name)
-                     
-        self.root.add_widget(view)
-        self.transition.direction = 'left'
-        self.root.current = view.name
        
-
 if __name__ == "__main__":
     GoodChoicesApp().run()
